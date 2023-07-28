62ad92cb823fa3ef5c3282e2092adaf800da88d6
juanjogg1987 <juanjogg1987@gmail.com>
2023-07-28 13:53:09 +0100

    new updated code Transfer Learning looking good!

--- GPyTorch_Models/New_Transfer_Learning_GP_with_GPU.py
+++ GPyTorch_Models/New_Transfer_Learning_GP_with_GPU.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 from gpytorch.constraints import Positive
 
 class TL_Kernel(gpytorch.kernels.Kernel):
-    # the sinc kernel is stationary
+    # the kernel is stationary
     is_stationary = True
 
     # We will register the parameter when initializing the kernel
@@ -30,11 +30,6 @@ class TL_Kernel(gpytorch.kernels.Kernel):
         # register the constraint
         self.register_constraint("raw_length", length_constraint)
 
-        #self.raw_length_list = [self.raw_length1,self.raw_length2] [exec("'self.raw_length'+str(i+1)) for i in range(Ntasks)"]
-        #self.raw_length_list = exec("['self.raw_length'+str(i+1) for i in range(Ntasks)]")
-
-        #self.length1 = self.raw_length1_constraint.transform(self.raw_length1)
-
         # set the parameter prior, see
         # https://docs.gpytorch.ai/en/latest/module.html#gpytorch.Module.register_prior
         if length_prior is not None:
@@ -51,12 +46,9 @@ class TL_Kernel(gpytorch.kernels.Kernel):
         # when accessing the parameter, apply the constraint transform
         return self.raw_length_constraint.transform(self.raw_length)
 
-    #['self.raw_length'+str(i+1) for i in range(Ntasks)]
-
     @length.setter
     def length(self, value):
         return self._set_length(value)
-
 
     def _set_length(self, value):
         if not torch.is_tensor(value):
@@ -67,20 +59,156 @@ class TL_Kernel(gpytorch.kernels.Kernel):
     # this is the kernel function
     def forward(self, x1, x2, idx1=None,idx2=None,square_dist=True, diag=False,**params):
         # apply lengthscale
-        A, B = torch.meshgrid(self.length[indx1].flatten() ** 2.0, self.length[indx2].flatten() ** 2.0)
-        #x1_ = x1.div(torch.sqrt(self.length[idx1]**2.0+self.length[idx2]**2.0))#/torch.sqrt(torch.tensor(2.0)))#.div(torch.sqrt(torch.tensor(2.0)))
-        #x2_ = x2.div(torch.sqrt(self.length[idx1]**2.0+self.length[idx2]**2.0))#/torch.sqrt(torch.tensor(2.0)))#.div(torch.sqrt(torch.tensor(2.0)))
-        #x1_ = x1.div(self.length[0])
-        #x2_ = x2.div(self.length[0])
+        if idx2 is None:
+            idx2 = idx1
+
+        A, B = torch.meshgrid(self.length[idx1].flatten() ** 2.0, self.length[idx2].flatten() ** 2.0)
         # calculate the distance between inputs
         diff = -1.0/(A+B) * self.covar_dist(x1, x2, square_dist=square_dist, diag=diag, **params)
-        #diff = -1.0*self.covar_dist(x1_, x2_, square_dist=square_dist, diag=diag,**params)
-        # prevent divide by 0 errors
-        #diff.where(diff == 0, torch.as_tensor(1e-20))
-        # return sinc(diff) = sin(diff) / diff
-        return diff.exp_() #torch.sin(diff).div(diff)
+        return diff.exp_()
 
 #gpytorch.kernels.RBFKernel()
+
+class TL_Kernel_var(gpytorch.kernels.Kernel):
+    # the kernel is stationary
+    is_stationary = True
+
+    # We will register the parameter when initializing the kernel
+    def __init__(self, NDomains,variance_prior=None,variance_constraint=None,length_prior=None, length_constraint=None, **kwargs):
+        super().__init__(**kwargs)
+        self.NDomains = NDomains
+
+        # set the parameter constraint to be positive, when nothing is specified
+        if length_constraint is None:
+            length_constraint = Positive()
+
+        if variance_constraint is None:
+            variance_constraint = Positive()
+
+        muDi_constraint = Positive()
+        bDi_constraint = Positive()
+        # register the raw parameter
+        self.register_parameter(
+            name='raw_length', parameter=torch.nn.Parameter(torch.zeros(*self.batch_shape, NDomains, 1))
+        )
+        self.register_parameter(
+            name='raw_variance', parameter=torch.nn.Parameter(torch.zeros(*self.batch_shape, NDomains, 1))
+        )
+        self.register_parameter(
+            name='raw_muDi', parameter=torch.nn.Parameter(torch.zeros(*self.batch_shape, NDomains, 1))
+        )
+        self.register_parameter(
+            name='raw_bDi', parameter=torch.nn.Parameter(torch.zeros(*self.batch_shape, NDomains, 1))
+        )
+        # register the constraint
+        self.register_constraint("raw_length", length_constraint)
+        self.register_constraint("raw_variance", variance_constraint)
+        self.register_constraint("raw_muDi", muDi_constraint)
+        self.register_constraint("raw_bDi", bDi_constraint)
+
+        # set the parameter prior, see
+        # https://docs.gpytorch.ai/en/latest/module.html#gpytorch.Module.register_prior
+        "The ifs below are just a template, but not already tested by Juan, so I do not know if it would work"
+        if length_prior is not None:
+            self.register_prior(
+                "length_prior",
+                length_prior,
+                lambda m: m.length,
+                lambda m, v : m._set_length(v),
+            )
+        if variance_prior is not None:
+            self.register_prior(
+                "variance_prior",
+                variance_prior,
+                lambda m: m.variance,
+                lambda m, v : m._set_variance(v),
+            )
+
+        self.alphaDi = 2.0*(1.0/(1.0+self.muDi)).pow(self.bDi) - 1.0
+
+    # now set up the 'actual' paramter
+    @property
+    def length(self):
+        # when accessing the parameter, apply the constraint transform
+        return self.raw_length_constraint.transform(self.raw_length)
+
+    @property
+    def variance(self):
+        # when accessing the parameter, apply the constraint transform
+        return self.raw_variance_constraint.transform(self.raw_variance)
+
+    @property
+    def muDi(self):
+        # when accessing the parameter, apply the constraint transform
+        return self.raw_muDi_constraint.transform(self.raw_muDi)
+
+    @property
+    def bDi(self):
+        # when accessing the parameter, apply the constraint transform
+        return self.raw_bDi_constraint.transform(self.raw_bDi)
+
+    @length.setter
+    def length(self, value):
+        return self._set_length(value)
+
+    @variance.setter
+    def variance(self, value):
+        return self._set_variance(value)
+
+    @muDi.setter
+    def muDi(self, value):
+        return self._set_muDi(value)
+
+    @bDi.setter
+    def bDi(self, value):
+        return self._set_bDi(value)
+
+    def _set_length(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_length)
+        # when setting the paramater, transform the actual value to a raw one by applying the inverse transform
+        self.initialize(raw_length=self.raw_length_constraint.inverse_transform(value))
+
+    def _set_variance(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_variance)
+        # when setting the paramater, transform the actual value to a raw one by applying the inverse transform
+        self.initialize(raw_variance=self.raw_variance_constraint.inverse_transform(value))
+
+    def _set_muDi(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_muDi)
+        # when setting the paramater, transform the actual value to a raw one by applying the inverse transform
+        self.initialize(raw_muDi=self.raw_muDi_constraint.inverse_transform(value))
+
+    def _set_bDi(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_bDi)
+        # when setting the paramater, transform the actual value to a raw one by applying the inverse transform
+        self.initialize(raw_bDi=self.raw_bDi_constraint.inverse_transform(value))
+
+    # this is the kernel function
+    def forward(self, x1, x2, idx1=None,idx2=None,square_dist=True, diag=False,**params):
+        # apply lengthscale
+        if idx2 is None:
+            idx2 = idx1
+
+        A, B = torch.meshgrid(self.length[idx1].flatten() ** 2.0, self.length[idx2].flatten() ** 2.0)
+        # Below the s2i and s2j are already treated as variances
+        s2i, s2j = torch.meshgrid(self.variance[idx1].flatten(), self.variance[idx2].flatten())
+        root_lengths = torch.sqrt(s2i*s2j)*torch.sqrt((2.0*torch.sqrt(A)*torch.sqrt(B))/(A+B))
+        # Compute the relatedness variables
+        self.alphaDi = 2.0 * (1.0 / (1.0 + self.muDi)).pow(self.bDi) - 1.0
+        lambdaDi, lambdaDj = torch.meshgrid(self.alphaDi[idx1].flatten(), self.alphaDi[idx2].flatten())
+        IndxDi, IndxDj = torch.meshgrid(torch.tensor(idx1), torch.tensor(idx2))
+        # calculate the distance between inputs
+        diff = -1.0/(A+B) * self.covar_dist(x1, x2, square_dist=square_dist, diag=diag, **params)
+
+        return root_lengths*diff.exp_()
+
+#gpytorch.kernels.RBFKernel()
+
+
 Nseed = 1
 torch.manual_seed(Nseed)
 import random
@@ -88,12 +216,16 @@ random.seed(Nseed)
 x1 = torch.randn(3,2)
 x2 = torch.randn(3,2)
 
-covar = TL_Kernel(NDomains= 3) #gpytorch.kernels.RBFKernel()
+"TODO run to find a matrix that is not squared, say x1 is got more data than x2!!!!!!!!!!!"
+
+covar = TL_Kernel_var(NDomains= 3) #gpytorch.kernels.RBFKernel()
 print(covar)
 covar._set_length([0.8,0.1,0.4])
+covar._set_variance([1,0.5,1.0])
+covar._set_muDi([0.3,0.5,1.0])
 indx1 = [0,1,2]
 indx2 = [0,1,2]
-MyK = covar(x1,x2,idx1=indx1,idx2=indx2).evaluate()
+MyK = covar(x1,idx1=indx1).evaluate()
 print(MyK)
 #We could try to build kernel per region: KSS, KTS and KTT, maybe use idea from the kernel index of MOGP
 #something like
